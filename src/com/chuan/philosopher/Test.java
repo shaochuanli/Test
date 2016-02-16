ff952cf6372dd3073282ad888f515ed28e056348
shaochuanli <371246840@qq.com>
2016-02-16 10:30:33 +0800

    add

---
+++ src/com/chuan/philosopher/Test.java
@@ -0,0 +1,17 @@
+package com.chuan.philosopher;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String[] names = { "骆昊", "王大锤", "张三丰", "杨过", "李莫愁" };   // 5位哲学家的名字
+		/*
+		 * ExecutorService es =
+		 * Executors.newFixedThreadPool(AppContext.NUM_OF_PHILO); // 创建固定大小的线程池
+		 * for(int i = 0, len = names.length; i < len; ++i) { es.execute(new
+		 * Philosopher(i, names[i])); // 启动线程 } es.shutdown();
+		 */
+        for(int i = 0, len = names.length; i < len; ++i) {
+            new Thread(new Philosopher(i, names[i])).start();
+        }
+	}
+}
