e5ae7475e6d71f30e1117a8a3573f02bbf02e1bf
shaochuanli <371246840@qq.com>
2016-03-30 13:48:04 +0800

    commit

--- src/com/chuan/test/Test.java
+++ src/com/chuan/test/Test.java
@@ -1,6 +1,7 @@
 package com.chuan.test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -8,12 +9,31 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.chuan.sort.KuaiSu;
 
 public class Test {
 
+	public static void ks(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length - i - 1; j++) {
+				if (array[j] > array[j + 1]) {
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		int[] array = {};
-		Object o = new Object();
+		int[] array = { 9, 15, 3, 7, 2, 1, 7, 3 };
+		Test.ks(array);
+		System.out.println(Arrays.toString(array));
+
+		// int[] array = {};
+		Object[] o = new Object[] { 1, 2 };
 		ArrayList[] arrayList = new ArrayList[8];
 		Vector<String> vector = new Vector<String>();
 		List<String> list = new ArrayList<String>();
@@ -23,5 +43,6 @@ public class Test {
 		TreeMap<String, Object> treeMap = new TreeMap<String, Object>();
 
 		Map<String, String> map = Collections.<String, String> synchronizedMap(hashMap);
+		ConcurrentHashMap conMap = new ConcurrentHashMap();
 	}
 }
